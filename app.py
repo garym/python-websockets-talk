0ffc1ddfcd921e568db60706d56d873ac0098d48
Gary Martin <gary.martin@physics.org>
2015-02-23 00:33:28 +0000

    Change to count of current connections

--- app.py
+++ app.py
@@ -2,19 +2,41 @@
 
 import redis
 
+from tornado.escape import json_encode
 from tornado.ioloop import IOLoop
 from tornado.web import Application, RequestHandler, url
+from tornado.websocket import WebSocketHandler
 
 db = redis.StrictRedis('db', 6379, 0)
 db.set('connections', 0)
 
-class ConnectionsHandler(RequestHandler):
+class ConnectionsPageHandler(RequestHandler):
     def get(self):
-        connections = db.incr('connections')
+        connections = db.get('connections')
         self.render("templates/connections.html", connections=connections)
 
+
+class ConnectionsHandler(WebSocketHandler):
+    handlers = []
+
+    def open(self):
+        self.handlers.append(self)
+        connections = db.incr('connections')
+        self.broadcast_data({'connections': connections})
+
+    def on_close(self):
+        self.handlers.remove(self)
+        connections = db.decr('connections')
+        self.broadcast_data({'connections': connections})
+
+    def broadcast_data(self, data):
+        for handler in self.handlers:
+            handler.write_message(data)
+
+
 def make_app():
-    return Application([url(r"/", ConnectionsHandler),
+    return Application([url(r"/", ConnectionsPageHandler),
+                        url(r"/socket/", ConnectionsHandler),
                        ], autoreload=True)
   
 def main():
